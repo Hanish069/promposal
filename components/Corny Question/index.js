e33e24de07f6aa1ac30994b0d02be5a54d4b6b61
Hanish Kumar <107493886+Hanish069@users.noreply.github.com>
2026-04-24 00:45:22 +0530

    Refactor CornyQuestion component layout and styles

--- components/Corny Question/index.js
+++ components/Corny Question/index.js
@@ -1,38 +1,30 @@
 import ToggledImage from '../Toggled Image'
 
-const CornyQuestion = ({ question, active, nonactive, secondary, left }) =>
-{
-    return (
-        <>
-    
-            { secondary ? 
-                <div className={left ? 
-                    "flex flex-row-reverse justify-around items-center text-center w-full h-96 bg-pink-200 p-5" :
-                    "flex flex-row justify-around items-center text-center w-full h-96 bg-pink-200 p-5"
-                }>
-                    <ToggledImage 
-                        nonactive={ nonactive }
-                        active={ active }
-                    />
-                    <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>{ question }</h1>
-                </div>
+const CornyQuestion = ({ question, active, nonactive, secondary, left }) => {
+  return (
+    <>
+      <div
+        className={
+          left
+            ? `flex flex-col md:flex-row-reverse justify-around items-center text-center w-full 
+               ${secondary ? "bg-pink-200" : "bg-pink-50"} p-5 md:p-10 gap-5`
+            : `flex flex-col md:flex-row justify-around items-center text-center w-full 
+               ${secondary ? "bg-pink-200" : "bg-pink-50"} p-5 md:p-10 gap-5`
+        }
+      >
+        <ToggledImage 
+          nonactive={nonactive}
+          active={active}
+        />
 
-                :
-
-                <div className={left ? 
-                    "flex flex-row-reverse justify-around items-center text-center w-full h-96 bg-pink-50 p-5" :
-                    "flex flex-row justify-around items-center text-center w-full h-96 bg-pink-50 p-5"
-                }>
-                    <ToggledImage 
-                        nonactive={ nonactive }
-                        active={ active }
-                    />
-                    <h1 className='w-2/3 text-pink-300 font-bold text-6xl'>{ question }</h1>
-                </div>
-            }
-
-        </>
-    )
+        <h1 className={`w-full md:w-2/3 font-bold 
+          text-2xl sm:text-3xl md:text-6xl 
+          ${secondary ? "text-pink-400" : "text-pink-300"}`}>
+          {question}
+        </h1>
+      </div>
+    </>
+  )
 }
 
-export default CornyQuestion;
+export default CornyQuestion
